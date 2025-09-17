8faf228dec85061159d3654ef51544750f4feff9
zeltina99 <minec369@gmail.com>
2025-09-17 14:20:59 +0900

    교수님과 간단 실습 완

--- HelloWorld/Day0917.h
+++ HelloWorld/Day0917.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <type_traits>
+
+
 class Day0917
 {
 
@@ -28,7 +31,32 @@ enum class PlayerState : uint8_t
 	Flying = 1 << 6
 };
 
+// PlayerState의 원래 타입을 Statetype이라고 하자고 정하는 것(별명 붙여주기)
+// using Statetype = unit8_t;
+// std::underlying_type_t는 #include <type_traits> 필요
+using Statetype = std::underlying_type_t<PlayerState>;
+
 inline PlayerState operator | (PlayerState Left, PlayerState Right)
 {
-	return static_cast<PlayerState>(static_cast<int>(Left) | static_cast<int>(PlayerState::Right));
+	return static_cast<PlayerState>(static_cast<Statetype>(Left) | static_cast<Statetype>(Right));
+}
+inline PlayerState operator&(PlayerState Left, PlayerState Right)
+{
+	return static_cast<PlayerState>(static_cast<Statetype>(Left) & static_cast<Statetype>(Right));
+}
+inline PlayerState operator~(PlayerState Value)
+{
+	return static_cast<PlayerState>(static_cast<Statetype>(Value));
+}
+
+inline PlayerState& operator|=(PlayerState& Left, PlayerState Right)
+{
+	Left = Left | Right;
+	return Left;
+}
+
+inline PlayerState& operator&=(PlayerState& Left, PlayerState Right)
+{
+	Left = Left & Right;
+	return Left;
 }

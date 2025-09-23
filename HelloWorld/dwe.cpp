ef5b6c33219e5e9b353254a4e0439a0bd01ad8f6
zeltina99 <minec369@gmail.com>
2025-09-23 17:14:25 +0900

    실습 끝

--- HelloWorld/dwe.cpp
+++ HelloWorld/dwe.cpp
@@ -1686,7 +1686,7 @@ int main() // 엔트리 포인트(코드가 시작되는 곳)
 	//day0923.TestTemplateClass();
 	//day0923.TestCalculator();
 	//day0923.TestVector();
-	day0923.TestVectorPractice();
+	//day0923.TestVectorPractice();
 
 
 	return 0;
